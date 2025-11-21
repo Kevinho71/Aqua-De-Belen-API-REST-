1895ed93adb25ee1b759659802d5f2a5ee726675
Kevinho71 <kevin.guzman@ucb.edu.bo>
2025-11-21 09:13:33 -0400

    Fix static resources configuration for Spring Boot 3

--- src/main/java/com/perfumeria/aquadebelen/aquadebelen/config/WebConfig.java
+++ src/main/java/com/perfumeria/aquadebelen/aquadebelen/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.perfumeria.aquadebelen.aquadebelen.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
